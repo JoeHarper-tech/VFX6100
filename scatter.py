6a0726fb95a22fa10ebd276507410ae7be734956
Joe Harper <jh18crt@bolton.ac.uk>
2024-11-20 11:47:50 +0000

    Update scatter.py

--- scatter.py
+++ scatter.py
@@ -4,44 +4,41 @@ from PySide2 import QtWidgets
 import hou
 import json
 import os
+import random
 
 class Scatter(QtWidgets.QWidget):
     def __init__(self, parent=None):
 
-        QtWidgets.QWidget.__init__(self, parent)            ## This initiates the UI
-        self.setWindowTitle('Scatter')            ##this sets the window title to "scatter"
+        QtWidgets.QWidget.__init__(self, parent)
+        self.setWindowTitle('Scatter')
         self.vBox = QtWidgets.QVBoxLayout()
-
 ##------------------------------------Scatter Object--------------------------------------------##
 
-        hbox1 = QtWidgets.QHBoxLayout()        ##this creates a layout, everything set within "hbox1" will be next to eachother horizontally
+        hbox1 = QtWidgets.QHBoxLayout()
 
-        self.labelScatterObj = QtWidgets.QLabel('Which object do you want to scatter?', self)            ##this adds a label
-        self.labelScatterObj.setMinimumWidth(175)             ##this defines the sie of the label
-        hbox1.addWidget(self.labelScatterObj)             ##this adds the label to the hbox1 layout
+        self.labelScatterObj = QtWidgets.QLabel('Which object do you want to scatter?', self)
+        self.labelScatterObj.setMinimumWidth(175)
+        hbox1.addWidget(self.labelScatterObj)
 
-##                 ADD THE DROPDOWN OPTIONS!!!!!!!
-        self.ddlScatterObj = QtWidgets.QComboBox(self)
-        self.ddlScatterObj.setMinimumWidth(175)
-        self.ddlScatterObj.addItem("")
-        hbox1.addWidget(self.ddlScatterObj)
+        self.textInputScatterObj = QtWidgets.QLineEdit(self)
+        self.textInputScatterObj.setMinimumWidth(175)
+        hbox1.addWidget(self.textInputScatterObj)
         self.vBox.addLayout(hbox1)
 
 ##------------------------------------Scatter Plane---------------------------------------------##
-
+        '''
         hbox2 = QtWidgets.QHBoxLayout()
 
         self.labelScatterPlane = QtWidgets.QLabel('Select the plane you want to scatter on', self)
         self.labelScatterPlane.setMinimumWidth(175)
         hbox2.addWidget(self.labelScatterPlane)
 
-##               ADD THE DROPDOWN OPTIONS!!!!!!
         self.ddlScatterPlane = QtWidgets.QComboBox(self)
         self.ddlScatterPlane.setMinimumWidth(175)
         self.ddlScatterPlane.addItem("")
         hbox2.addWidget(self.ddlScatterPlane)
         self.vBox.addLayout(hbox2)
-
+        '''
 ##-------------------------------------Scatter Num-------------------------------------------------##
 
         hbox3 = QtWidgets.QHBoxLayout()
@@ -79,9 +76,55 @@ class Scatter(QtWidgets.QWidget):
         hbox4.addWidget(self.textInputRotationZ)
         self.vBox.addLayout(hbox4)
 
-##-------------------------------------SnapToGround-------------------------------------##
+        
+##--------------------------------Translation upper bounds-----------------------------------##
+        hbox5 = QtWidgets.QHBoxLayout()
+        self.labelTranslationLower = QtWidgets.QLabel('what are the lower bounds of translation you would like? (x,y,z)')
+        self.labelTranslationLower.setMinimumWidth(175)
+        hbox5.addWidget(self.labelTranslationLower)
 
-        hbox5 = QtWidgets.QHboxLayout()
+        self.textInputTranslationLowerX = QtWidgets.QLineEdit(self)
+        self.textInputTranslationLowerX.setMinimumWidth(50)
+        self.textInputTranslationLowerX.textChanged.connect(self.textHasChangedTranslationLowerX)
+
+        self.textInputTranslationLowerY = QtWidgets.QLineEdit(self)
+        self.textInputTranslationLowerY.setMinimumWidth(50)
+        self.textInputTranslationLowerY.textChanged.connect(self.textHasChangedTranslationLowerY)
+
+        self.textInputTranslationLowerZ = QtWidgets.QLineEdit(self)
+        self.textInputTranslationLowerZ.setMinimumWidth(50)
+        self.textInputTranslationLowerZ.textChanged.connect(self.textHasChangedTranslationLowerZ)
+
+        hbox5.addWidget(self.textInputTranslationLowerX)
+        hbox5.addWidget(self.textInputTranslationLowerY)
+        hbox5.addWidget(self.textInputTranslationLowerZ)
+        self.vBox.addLayout(hbox5)
+##--------------------------------Translation lower bounds-----------------------------------##
+        hbox6 = QtWidgets.QHBoxLayout()
+        self.labelTranslationUpper = QtWidgets.QLabel('what are the upper bounds of translation you would like? (x,y,z)')
+        self.labelTranslationUpper.setMinimumWidth(175)
+        hbox6.addWidget(self.labelTranslationUpper)
+
+        self.textInputTranslationUpperX = QtWidgets.QLineEdit(self)
+        self.textInputTranslationUpperX.setMinimumWidth(50)
+        self.textInputTranslationUpperX.textChanged.connect(self.textHasChangedTranslationUpperX)
+        
+        self.textInputTranslationUpperY = QtWidgets.QLineEdit(self)
+        self.textInputTranslationUpperY.setMinimumWidth(50)
+        self.textInputTranslationLowerY.textChanged.connect(self.textHasChangedTranslationUpperY)
+
+        self.textInputTranslationUpperZ = QtWidgets.QLineEdit(self)
+        self.textInputTranslationUpperZ.setMinimumWidth(50)
+        self.textInputTranslationUpperZ.textChanged.connect(self.textHasChangedTranslationUpperZ)
+
+        hbox6.addWidget(self.textInputTranslationUpperX)
+        hbox6.addWidget(self.textInputTranslationUpperY)
+        hbox6.addWidget(self.textInputTranslationUpperZ)
+        self.vBox.addLayout(hbox6)
+
+##-------------------------------------SnapToGround-------------------------------------##
+        '''
+        hbox5 = QtWidgets.QHBoxLayout()
         
         self.cbSnapToGround = QtWidgets.QCheckBox("SnapToGround", self)
         self.cbSnapToGround.setMinimumWidth(150)
@@ -89,17 +132,23 @@ class Scatter(QtWidgets.QWidget):
         
         hbox5.addWidget(self.cbSnapToGround)
         self.vBox.addLayout(hbox5)
-
+        '''
 ##---------------------------------------------Button----------------------------------##
 
-        hbox6 = QtWidgets.QHBoxLayout()
-        self.button = QtWidgets.QPushButton("Build", self)
-        hbox6.addWidget(self.button)
-        self.vBox.addLayout(hbox6)
+        hbox7 = QtWidgets.QHBoxLayout()
+        self.button = QtWidgets.QPushButton('Build', self)
+        self.button.clicked.connect(self.buildProject)
+        hbox7.addWidget(self.button)
+        self.vBox.addLayout(hbox7)
+
+      
 
 ##--------------------------------------------------------------------------------------##
         self.setLayout(self.vBox)
 
+
+    def textHasChangedScatterObj(self):
+        scatterObjText = self.textInputScatterObj.text()
 
     def textHasChangedScatterNum(self):
         scatterNum = self.textInputScatterNum.text()
@@ -112,9 +161,85 @@ class Scatter(QtWidgets.QWidget):
 
     def textHasChangedRotationZ(self):
         rotationZ = self.textInputRotationZ.text()
+        
+    def textHasChangedTranslationLowerX(self):
+        translationLowerX = self.textInputTranslationLowerX
 
-    def stateHasChangedSnapToGround(self):
-        snapToGround = self.cbSnapToGround()
+    def textHasChangedTranslationLowerY(self):
+        translationLowerY = self.textInputTranslationLowerY
+
+    def textHasChangedTranslationLowerZ(self):
+        translationLowerZ = self.textInputTranslationLowerZ
+
+    def textHasChangedTranslationUpperX(self):
+        translationUpperX = self.textInputTranslationUpperX
+
+    def textHasChangedTranslationUpperY(self):
+        translationUpperY = self.textInputTranslationUpperY
+
+    def textHasChangedTranslationUpperZ(self):
+        translationUpperZ = self.textInputTranslationUpperZ
+
+
+    def buildProject(self):
+
+
+        scatterObjText = self.textInputScatterObj.text()
+        scatterNumText = self.textInputScatterNum.text()
+        rotationXText = self.textInputRotationX.text()
+        rotationYText = self.textInputRotationY.text()
+        rotationZText = self.textInputRotationZ.text()
+        translationLowerX = self.textInputTranslationLowerX.text()
+        translationLowerY = self.textInputTranslationLowerY.text()
+        translationLowerZ = self.textInputTranslationLowerZ.text()
+        translationUpperX = self.textInputTranslationUpperX.text()
+        translationUpperY = self.textInputTranslationUpperY.text()
+        translationUpperZ = self.textInputTranslationUpperZ.text()
+
+       # print(scatterObjText)
+
+        scatterObj = hou.node(f'/obj/geo1/{scatterObjText}')
+        obj = hou.node("/obj")
+        myGeo = hou.node('/obj/geo1')
+        timesRan = 0
+        subnet = myGeo.createNode("subnet", 'scatter')
+        mySub = hou.node('/obj/geo1/scatter/')
+        merge = mySub.createNode("merge", 'merge1')
+        subnet.setInput(0, scatterObj)
+
+        for i in range(int(scatterNumText)):
+                timesRan+=1
+                randomRotationX = random.randint (0, (int(rotationXText)))
+                randomRotationY = random.randint (0, (int(rotationYText)))
+                randomRotationZ = random.randint (0, (int(rotationZText)))
+                randomTranslationX = random.randint ((int(translationLowerX)), (int(translationUpperX)))
+                randomTranslationY = random.randint ((int(translationLowerY)), (int(translationUpperY)))
+                randomTranslationZ = random.randint ((int(translationLowerZ)), (int(translationUpperZ)))
+                
+                copy = mySub.createNode("copyxform", f'copy{timesRan}')
+                copyNum = copy.parm('ncy')
+                copyTranslateX = copy.parm('tx')
+                copyTranslateY = copy.parm('ty')
+                copyTranslateZ = copy.parm('tz')
+                copyRotateX = copy.parm('rx')
+                copyRotateY = copy.parm('ry')
+                copyRotateZ = copy.parm('rz')
+                copyNum.set(2)
+                copyTranslateX.set(randomTranslationX)
+                copyTranslateY.set(randomTranslationY)
+                copyTranslateZ.set(randomTranslationZ)
+                copyRotateX.set(randomRotationX)
+                copyRotateY.set(randomRotationY)
+                copyRotateZ.set(randomRotationZ)
+                copy.setInput(0, mySub.indirectInputs()[0])
+                merge.setInput(timesRan, copy)
+
+                print(scatterObj)
+                print(randomRotationX, randomRotationY, randomRotationZ)
+                print(randomTranslationX, randomTranslationY, randomTranslationZ)
+                print(timesRan)
+
+
 
 dialog = Scatter()
 dialog.show()
